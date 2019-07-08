e94c0112418f8d0df19c626289aff9b80b0cabab
Paul Tibbetts <paul.tibbetts@stickee.co.uk>
2019-07-08 15:21:20 +0100

    Initial release

---
+++ dist/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['stickeepaul'],
+    // settings: {
+    //     "import/resolver": {
+    //         "webpack": {
+    //             "config": "build/webpack.base.conf.js"
+    //         }
+    //     }
+    // },
+    // rules: {
+    //     ...
+    // }
+}
